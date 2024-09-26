9fddebb93c74a3d980e76e62b5c38372a38b79f6
MOHITHRAM <mohithram266@gmail.com>
2024-09-26 21:45:54 +0530

    Add files via upload

---
+++ JS file/script.js
@@ -0,0 +1,47 @@
+var msg ="Mohith"
+var msg ="Hello my name is mohith, I'm learning js";
+
+// Alearting
+alert(msg);
+
+
+//Logging
+console.log("msg"); 
+
+let msg ="Mohith"
+let msg ="Hello my name is mohith, I'm learning js";
+
+// Alearting
+alert(msg);
+
+
+//Logging
+console.log("msg"); 
+
+  let number5 ="5";
+  console.log(result);
+
+  const name = "Mohith";
+  const dob = "30-05-2001";
+
+  let msg ="Hello my name is " + name + " i born on " + dob + " I'm learning js"
+  
+  /loggging
+  console.log(msg);
+
+ /*
+     synchronous & Asynchronous| js
+ /*
+ 
+ console.log("\n synchronous process ");
+ console.log("js-1");
+ console.log("js-2");
+ console.log("js-3");
+
+
+ console.log("\n Asynchronous process ");
+ console.log("js-1");
+ setTimeout(()= >{
+   console.log("js-2");
+ },1000);
+ console.log(js-3);
