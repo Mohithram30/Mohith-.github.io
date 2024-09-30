7c445d170aada6bf8e0516bc36faad864bdd9a8e
MOHITHRAM <mohithram266@gmail.com>
2024-09-30 07:14:15 +0530

    Add files via upload

---
+++ js/Execution Phase.js
@@ -0,0 +1,8 @@
+let a = 10;
+
+function foo() {
+  let b = 20;
+  console.log(a + b);
+}
+
+foo();
---
+++ js/if-else.js
@@ -0,0 +1,7 @@
+let num = 3;
+
+if (num > 5) {
+  console.log("Number is greater than 5");
+} else {
+  console.log("Number is 5 or less");
+}
