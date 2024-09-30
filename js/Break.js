3c6cca887a32d5fd9f215acb97afbba5d53a2dd5
MOHITHRAM <mohithram266@gmail.com>
2024-09-30 07:18:04 +0530

    Update Break.js

--- js/Break.js
+++ js/Break.js
@@ -2,4 +2,6 @@ for (let i = 0; i < 5; i++) {
     if (i === 3) break;
     console.log(i);
   }
-  
+
+If num is greater than 5, the code inside the if block is executed.
+The if statement only runs the block when the condition evaluates to true.
